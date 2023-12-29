1451eb3db0f24ec8e94121e16b5343541ba91135
Willmuseve <willmuseve@gmail.com>
2023-12-29 03:16:01 -0800

    task 9 updates

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -13,19 +13,19 @@ app = Flask(__name__)
 def get_state_name(state):
     return state.name
 
-@app.route('/states_list', strict_slashes=False)
-def list_of_states():
+def list_states():
     """
-    Renders an HTML template that lists all states
+     Displays an HTML page with a list of all State objects in DBStorage.
     """
-    states = storage.all(State).values()
-    states = sorted(states, key=get_state_name)
+    states = storage.all("State")
+    return render_template("7-states_list.html", states=states)
 
-    return render_template('7-states_list.html', states=states)
 
 @app.teardown_appcontext
-def teardown(exception):
+def teardown(exc):
+    """Remove the current session."""
     storage.close()
 
-if __name__ == '__main__':
-    app.run(port=5000, host='0.0.0.0')
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
