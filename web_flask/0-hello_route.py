a91b781bc20537f610e9a19d37ed74d19ae21941
Willmuseve <willmuseve@gmail.com>
2023-12-29 01:24:53 -0800

    task 1 hello flask

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+"""
+Starting a simple flask application
+"""
+
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def greet_hbnb():
+    """
+    a function that returns 'Hello HBNB'.
+    """
+    return "Hello HBNB!"
+
+
+if __name__ == '__main__':
+    app.run(port=5000, host='0.0.0.0')
