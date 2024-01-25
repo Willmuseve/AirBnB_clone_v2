dc34a092b365efe46edfe971bfd46e5200dd7cc3
willmuseve <willmuseve@gmail.com>
2024-01-25 19:48:43 +0000

    app server

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -7,10 +7,10 @@ Starting a simple flask application
 
 from flask import Flask, render_template
 
-app = Flask"(__name__")
+app = Flask(__name__)
 
 
-@app.route('/', strict_slashes=False)
+@app.route('/airbnb-onepage/', strict_slashes=False)
 def greet_hbnb():
     """
     a function that returns 'Hello HBNB'.
